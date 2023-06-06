be5cd010e366686f658812a2ee48fdbbf826b7c8
Jhonattan Mercado <jmercadosenior@gmail.com>
2023-06-06 14:58:08 -0500

    aula 1 actividad 7  finalizada

--- src/components/FormSingUp.jsx
+++ src/components/FormSingUp.jsx
@@ -1,8 +1,17 @@
+import { Button } from "@mui/material"
+
 function FormSingUp () {
     return (
     <form>
+        <label>Nombre</label>
         <input type="text" />
-        <button>Registrarse</button>
+        <label>Apellidos</label>
+        <input type="text" />
+        <label>Correo Electronico</label>
+        <input type="email" />
+        <label>Promociones</label>
+        <input type="checkbox" />
+        <Button variant="contained">Registrarse</Button>
     </form>
     )
 }
