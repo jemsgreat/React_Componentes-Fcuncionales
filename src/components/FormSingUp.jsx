e5f3b62073763dca8dd1ecebd4f2f3bf1e7f1bfd
Jhonattan Mercado <jmercadosenior@gmail.com>
2023-06-06 17:17:57 -0500

    aula 3  actividad 2 y 5  finalizada

--- src/components/FormSingUp.jsx
+++ src/components/FormSingUp.jsx
@@ -3,8 +3,13 @@ import TextField from '@mui/material/TextField';
 import Switch from "@mui/material/Switch";
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import { useEffect, useState } from "react";
 
 function FormSingUp () {
+    const [name, setName] = useState("")
+    useEffect(() =>{  /* podemos usar useEffect para escuchar los cambios del estado cuando sean hechos, como si fuera tiempo real, mientras que el useState no los muestra enseguida */
+        console.log("name cambio: ", name)
+    }, [name])
     return (
     <form>
         <TextField 
@@ -13,6 +18,11 @@ function FormSingUp () {
             variant="outlined" 
             fullWidth
             margin="normal"
+            onChange={(e) =>{
+                console.log(e.target.value)
+                setName(e.target.value)
+            }}
+            value={name}
         />
         <TextField 
             id="lastName" 
