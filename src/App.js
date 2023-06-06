ac499c279e7c933ab69e4d446a41ae49cfcf84de
Jhonattan Mercado <jmercadosenior@gmail.com>
2023-06-06 16:52:15 -0500

    aula 2  actividad 2,3 y 5  finalizada

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+
+import { Container } from '@mui/material';
 import './App.css';
 import FormSingUp from './components/FormSingUp';
+import Typography from '@mui/material/Typography';
 
 
 
 function App() {
   return (
-    <>
-      <h1>Formulario Registro</h1>
+    <Container component="section" maxWidth="md">
+      <Typography variant="h3" align="center" component="h1">Formulario Registro</Typography>
       <FormSingUp />
-    </>      
+    </Container>      
   );
 }
 
--- src/components/FormSingUp.jsx
+++ src/components/FormSingUp.jsx
@@ -1,16 +1,48 @@
-import { Button } from "@mui/material"
+import { Button } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import Switch from "@mui/material/Switch";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 function FormSingUp () {
     return (
     <form>
-        <label>Nombre</label>
-        <input type="text" />
-        <label>Apellidos</label>
-        <input type="text" />
-        <label>Correo Electronico</label>
-        <input type="email" />
-        <label>Promociones</label>
-        <input type="checkbox" />
+        <TextField 
+            id="name" 
+            label="Nombre" 
+            variant="outlined" 
+            fullWidth
+            margin="normal"
+        />
+        <TextField 
+            id="lastName" 
+            label="Apellidos" 
+            variant="outlined" 
+            fullWidth
+            margin="normal"
+        />
+        <TextField 
+            id="email" 
+            label="Email" 
+            variant="outlined" 
+            fullWidth
+            margin="normal"
+        />
+        <FormGroup>
+            <FormControlLabel 
+                control={
+                    <Switch defaultChecked/>
+                }
+                label="promociones"
+            />  
+            <FormControlLabel 
+                control={
+                    <Switch defaultChecked/>
+                }
+                label="Novedades"
+            />    
+        </FormGroup>
+        
         <Button variant="contained">Registrarse</Button>
     </form>
     )
